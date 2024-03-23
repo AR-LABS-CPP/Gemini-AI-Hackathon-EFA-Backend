f237103b3be8a3a058df09f6b4d4fcbeca23d57c
Ali Raza <70814565+AR-LABS-CPP@users.noreply.github.com>
2024-03-23 22:23:41 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 
 const connectDB = require("./config/mongo.config")
 const express = require("express")
+const cors = require("cors")
 const morgan = require("morgan")
 const app = express()
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3040
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 app.use(morgan("short"))
 
 const userRouter = require("./routes/user.routes")
